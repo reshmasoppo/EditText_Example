945b8de74943ab865a0a8dea660530d7b9b6f4f2
IN007848 <Reshma@oppo2019>
2019-11-04 11:43:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/edittext_example/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.edittext_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /* //Create a new EditText Widget programmatically
+        var edit_text=EditText(this)
+        edit_text.layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        edit_text.hint = "Enter Text"
+        // add EditText to constraint layout
+        cont.addView(edit_text)
+
+
+    }*/
+
+    }
+}
